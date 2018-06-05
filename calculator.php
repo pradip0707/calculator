5c0debf6301f1d0d556e9013cc173ddeec5cf3c0
Nitin Wagh <nitin.wagh@fdsinfotech.com>
2018-06-05 18:31:06 +0530

    [TASK] first commit

---
+++ calculator.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Library\Calculator;
+
+$calculator = new Calculator();
+
+if (!isset($argv[1])) {
+    echo 'Please provide operator, "sum" or "multiply"' . PHP_EOL;
+    return 0;
+}
+
+$data = $argv[2] ?? '';
+
+print_r($data);
+
+try {
+    $operator = strtolower($argv[1]);
+    switch ($operator) {
+        case 'sum':
+            $output = $calculator->add($data);
+            break;
+        case 'multiply':
+            $output = $calculator->multiply($data);
+            break;
+        default:
+            $output = 'Please provide valid operator, "sum" or "multiply"';
+            break;
+    }
+    echo $output . PHP_EOL;
+} catch (\InvalidArgumentException $e) {
+    echo "Error: " . $e->getMessage() . PHP_EOL;
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,30 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Library\Calculator;
+
+class CalculatorTest extends TestCase {
+
+    /**
+     * Calculator
+     */
+    protected $calculator;
+
+    /**
+     * Basic setup for test cases
+     */
+    public function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+
+    /**
+     * Test sum zero to two numbers
+     *
+     * @dataProvider dataForSumFromZeroToTwoNumbers
+     */
+    public function testSumtZeroToTwoNumbers($data, $sum)
+    {
+        $this->assertEquals($sum, $this->calculator->add($data));
+    }
+
+}
